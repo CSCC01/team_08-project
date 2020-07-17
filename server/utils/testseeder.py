5559b46113febe04b69ed36517f49d02dfb631ed
Robert Nichita <robert.nickykitta@gmail.com>
2020-07-17 17:51:05 -0400

    Created seeding framework and scripts to seed restaurants and foods

--- server/utils/testseeder.py
+++ server/utils/testseeder.py
@@ -16,6 +16,8 @@ class AddTestCase(SimpleTestCase):
     def test_add_randomizer(self):
         '''
         dictionary correctly adds random generation functions
+        take the same function and manually insert it into the dictionary in the same way
+        that the add_randomizer function is supposed to
         '''
         gen_dict = {}
         testfunc = lambda x: x.name()
@@ -23,19 +25,23 @@ class AddTestCase(SimpleTestCase):
         self.seeder.add_randomizer("name", testfunc, gen_dict)
         self.assertEqual(gen_dict["name"], expected["name"])
 
+
     def test_gen_rand_dict(self):
         '''
         JSON dump is correctly randomly generated
+        This test works by comparing the random output at the same seed value, so the output will be identical
         '''
         gen_dict = {"name": lambda q: q.name(), "address" : lambda x: x.address}
         expected_document = {"name": self.seeder.faker.name(), "address" : self.seeder.faker.address}
-        document = self.seeder.gen_rand_dict(0,gen_dict)
+        Faker.seed(0)
+        document = self.seeder.gen_rand_dict(gen_dict)
         self.assertEqual(document["name"], expected_document["name"])
         self.assertEqual(document["address"], expected_document["address"])
 
     def test_clean_dict(self):
         '''
         dicts are properly cleaned of invalid functions
+        The lambda function cannot be encoded to json by python's json library so it is cleaned, while the name is valid 
         '''
         testfuncinvalid = lambda faker : lambda faker : None
         testfuncvalid = lambda faker : faker.name()
