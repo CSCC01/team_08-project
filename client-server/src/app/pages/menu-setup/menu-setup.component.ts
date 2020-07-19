ab8b00762d85f5067ae35d980a8231128fb40f94
Winn <winnieee.lam00@gmail.com>
2020-07-18 22:09:43 -0400

    SD-124: Added checks and alerts for invalid/incomplete input

--- client-server/src/app/pages/menu-setup/menu-setup.component.ts
+++ client-server/src/app/pages/menu-setup/menu-setup.component.ts
@@ -44,28 +44,43 @@ export class MenuSetupComponent implements OnInit {
   }
 
   addDish() {
-    const price: number = +this.price;
-    //TODO: picture currently defaulted, will be changed when Google Cloud is implemented
-    var dishInfo = {
-      name: this.dishName,
-      restaurant_id: this.restaurantId,
-      description: this.dishInfo,
-      picture:
-        'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg',
-      price: price.toFixed(2),
-      specials: '',
-    };
+    if (
+      this.dishName == '' ||
+      this.price == '' ||
+      this.menuCategory == '' ||
+      this.cuisine == '' ||
+      this.dishInfo == '' ||
+      this.allergy == ''
+    ) {
+      alert('Please enter requried information about the dish!');
+    } else {
+      if (!isNaN(Number(this.price))) {
+        const price: number = +this.price;
+        //TODO: picture currently defaulted, will be changed when Google Cloud is implemented
+        var dishInfo = {
+          name: this.dishName,
+          restaurant_id: this.restaurantId,
+          description: this.dishInfo,
+          picture:
+            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg',
+          price: price.toFixed(2),
+          specials: '',
+        };
 
-    this.restaurantsService.createDish(dishInfo);
+        this.restaurantsService.createDish(dishInfo);
 
-    this.dishName = '';
-    this.price = '';
-    this.menuCategory = '';
-    this.cuisine = '';
-    this.dishInfo = '';
-    this.allergy = '';
+        this.dishName = '';
+        this.price = '';
+        this.menuCategory = '';
+        this.cuisine = '';
+        this.dishInfo = '';
+        this.allergy = '';
 
-    this.modalRef.close();
-    this.loadAllDishes();
+        this.modalRef.close();
+        this.loadAllDishes();
+      } else {
+        alert('Please enter a valid price!');
+      }
+    }
   }
 }
