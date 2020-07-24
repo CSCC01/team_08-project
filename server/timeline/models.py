befa1a503900e08931d91a6e0f3338da591f4656
alacwong <46137625+alacwong@users.noreply.github.com>
2020-07-24 12:24:19 -0400

    SD-145: Corrected fields to follow schema more closely

--- server/timeline/models.py
+++ server/timeline/models.py
@@ -1,6 +1,6 @@
 from djongo import models
 from bson import ObjectId
-from RO.models import Restaurant
+from restaurant.models import Restaurant
 from restaurant.models import Food
 from auth2.models import SDUser
 
@@ -8,17 +8,19 @@ from auth2.models import SDUser
 # Model for a post on an restaurant's timeline
 class TimelinePost(models.Model):
     _id = models.ObjectIdField()
-    restaurant = models.ForeignKey(Restaurant)
-    user = models.ForeignKey(SDUser)
-    likes = models.IntegerField(default = 0)
-    content = models.CharacterField(max_length = 3000)
+    restaurant_id = models.CharField(max_length=24)
+    user_id = models.CharField(max_length=24)
+    likes = models.ListField()
+    content = models.CharacterField(max_length=3000)
     Timestamp = models.DateTimeField()
+    comments = models.ListField()
+
 
 # Model for a comment on a timeline post
 class TimelineComment(models.Model):
     _id = models.ObjectIdField()
     post = models.ForeignKey(TimelinePost)
-    user = models.ForeignKey(SDUser)
-    likes = models.IntegerField()
-    content = models.CharField(max_length = 300)
+    user_id = models.CharField(max_length=24)
+    likes = models.ListField()
+    content = models.CharField(max_length=300)
     Timestamp = models.DateTimeField()
