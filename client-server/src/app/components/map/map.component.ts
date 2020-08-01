e51148484060220ceb397a3bfa12776097b7da51
Winn <winnieee.lam00@gmail.com>
2020-08-01 15:13:30 -0400

    SD-116: Added current location feature to center map

--- client-server/src/app/components/map/map.component.ts
+++ client-server/src/app/components/map/map.component.ts
@@ -1,7 +1,6 @@
 import { environment } from '../../../environments/environment';
 import { Component, OnInit, Input } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-map',
@@ -12,56 +11,71 @@ export class MapComponent implements OnInit {
   @Input() restaurants: any;
   @Input() userId: string;
   @Input() role: string;
-  popupDetails: any[] = [];
 
-  map: mapboxgl.Map;
-  style = 'mapbox://styles/mapbox/streets-v11';
   lat = 43.7839;
   lng = -79.1874;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor() {}
 
   ngOnInit() {
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
     Object.getOwnPropertyDescriptor(mapboxgl, 'accessToken').set(
       environment.mapbox.accessToken
     );
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: 13,
-      center: [this.lng, this.lat],
-    });
-    // Add map controls
-    this.map.addControl(new mapboxgl.NavigationControl());
 
-    // Add map markers
-    var marker = new mapboxgl.Marker({ color: '#FF0000' })
-      .setLngLat([this.lng, this.lat])
-      .setPopup(new mapboxgl.Popup().setHTML('<p>You are here!</p>'))
-      .addTo(this.map);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var style = 'mapbox://styles/mapbox/streets-v11';
+        var map: mapboxgl.Map;
 
-    for (var i = 0; i < this.restaurants.length; i++) {
-      var index = this.restaurants[i];
+        map = new mapboxgl.Map({
+          container: 'map',
+          style: style,
+          zoom: 13,
+          center: [position.coords.longitude, position.coords.latitude],
+        });
 
-      var GEOJson = JSON.parse(index.GEO_location.replace(/\'/g, '"'));
+        // Add map controls
+        map.addControl(new mapboxgl.NavigationControl());
 
-      if (index.GEO_location != 'blank' && GEOJson.long != undefined) {
-        var marker = new mapboxgl.Marker({ color: '#CB1E21' })
-          .setLngLat([GEOJson.long, GEOJson.lat])
-          .setPopup(
-            new mapboxgl.Popup().setHTML(
-              `<h2>${index.name}</h2>
-              ${index.address}
-              <br/>
-              ${index.phone}
-              <br/>
-              ${index.cuisine}
-              <br/>
-              <a class="btn" href="${environment.site_url}/restaurant?restaurantId=${index._id}&userId=${this.userId}&role=${this.role}"> View Restaurant </a>`
-            )
-          )
-          .addTo(this.map);
-      }
-    }
+        // Add map markers
+        var marker = new mapboxgl.Marker({ color: '#FF0000' })
+          .setLngLat([position.coords.longitude, position.coords.latitude])
+          .setPopup(new mapboxgl.Popup().setHTML('<p>You are here!</p>'))
+          .addTo(map);
+
+        for (var i = 0; i < this.restaurants.length; i++) {
+          var index = this.restaurants[i];
+          var GEOJson = JSON.parse(index.GEO_location.replace(/\'/g, '"'));
+
+          if (index.GEO_location != 'blank' && GEOJson.long != undefined) {
+            var marker = new mapboxgl.Marker({ color: '#CB1E21' })
+              .setLngLat([GEOJson.long, GEOJson.lat])
+              .setPopup(
+                new mapboxgl.Popup().setHTML(
+                  `<h2>${index.name}</h2>
+                  ${index.address}
+                  <br/>
+                  ${index.phone}
+                  <br/>
+                  ${index.cuisine}
+                  <br/>
+                  <a class="btn" href="${environment.site_url}/restaurant?restaurantId=${index._id}&userId=${this.userId}&role=${this.role}"> View Restaurant </a>`
+                )
+              )
+              .addTo(map);
+          }
+        }
+      },
+      this.error,
+      options
+    );
   }
+
+  error(err) {}
 }
