4016177eba2a3365c58055944f4469d31c0a866b
alacwong <46137625+alacwong@users.noreply.github.com>
2020-07-31 23:08:27 -0400

    SD-162: fixed urls

--- server/order/urls.py
+++ server/order/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('cart/insert/', views.insert_cart_page, name='insert_cart_page'),
-<<<<<<< HEAD
-    path('cart/update_status/', views.update_status_page, name='update_status_page')
-=======
+    path('cart/update_status/', views.update_status_page, name='update_status_page'),
     path('item/insert/', views.insert_item_page, name='insert_item_page'),
->>>>>>> develop
 ]
