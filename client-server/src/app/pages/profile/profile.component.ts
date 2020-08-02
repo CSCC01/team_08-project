8cf0d742f39f66deeaa876be225d0959e9d4f793
Tony Cao <tony.cao1@outlook.com>
2020-08-01 20:24:42 -0400

    bugfix: created edit user info modal

--- client-server/src/app/pages/profile/profile.component.ts
+++ client-server/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginService } from 'src/app/service/login.service';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +12,59 @@ import { LoginService } from 'src/app/service/login.service';
 })
 export class ProfileComponent implements OnInit {
   userId: string = '';
-  userInfo: any;
-  constructor(public auth: AuthService, private loginService: LoginService) {}
+  userData: any;
+  modalRef: any;
+  faCalendar = faCalendar;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public auth: AuthService,
+    private loginService: LoginService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.userId = sessionStorage.getItem('userId');
     this.loginService.getUser({ email: this.userId }).subscribe((data) => {
-      this.userInfo = data;
+      this.userData = data;
     });
+  }
+
+  openEditModal(content) {
+    this.modalRef = this.modalService.open(content);
+  }
+
+  updateProfile() {
+    var userInfo = {
+      email: this.userId,
+      name: (<HTMLInputElement>document.getElementById('name')).value,
+      address: (<HTMLInputElement>document.getElementById('address')).value,
+      phone: (<HTMLInputElement>document.getElementById('phone')).value,
+      birthday: (<HTMLInputElement>document.getElementById('dateOfBirth'))
+        .value,
+    };
+
+    if (userInfo.birthday == '') {
+      userInfo.birthday = null;
+    }
+    if (userInfo.phone == '') {
+      userInfo.phone = null;
+    }
+
+    if (
+      (userInfo.phone != null && userInfo.phone.length != 10) ||
+      (userInfo.birthday != null &&
+        !userInfo.birthday.match('^\\d{4}-\\d{2}-\\d{2}$')) ||
+      !userInfo.name
+    ) {
+      alert(
+        'Please ensure formats are proper. Name should not empty, phone numbers should be 10 digits with no dashes and birthday should be YYYY-MM-DD'
+      );
+    } else {
+      this.loginService.editUser(userInfo);
+      this.modalRef.close();
+      // window.location.reload();
+    }
   }
 }
