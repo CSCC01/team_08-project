30cdbc2efb23b27aecb72ed569bc69d1c8edcf76
Robert Nichita <robert.nickykitta@gmail.com>
2020-08-03 16:55:58 -0400

    SD-183: added decline order endpoint, testing refactored so that stubs are in a separate folder

--- server/order/views.py
+++ server/order/views.py
@@ -62,8 +62,18 @@ def update_status_page(request):
                 return JsonResponse(json.loads(json.dumps(model_to_dict(cart), cls=BSONEncoder)))
             except ValueError as error:
                 return HttpResponseBadRequest(str(error))
-    return HttpResponseBadRequest('Invalid request, please use check your request')
+    return HttpResponseBadRequest('Invalid request, please check your request')
 
+def decline_cart_page(request):
+    """Decline a cart which has been sent by a user"""
+    validate(instance=request.body, schema=cart_schema)
+    body = json.loads(request.body)
+    try:
+        cart = Cart.decline_cart(Cart, cart_id= body['_id'])
+        return JsonResponse(json.loads(json.dumps(model_to_dict(cart), cls=BSONEncoder)))
+    except ValueError as error:
+        return HttpResponseBadRequest(str(error))
+    return HttpResponseBadRequest('Invalid request, please check your request')
 
 
 def insert_item_page(request):
