f38fc941ef78d14aba6ba1cd02f401d9445ff926
Tony Cao <tony.cao1@outlook.com>
2020-08-06 18:20:08 -0400

    SD-76: Created component for the order card on the all orders page

--- client-server/src/app/app.module.ts
+++ client-server/src/app/app.module.ts
@@ -48,6 +48,7 @@ import { CartCardComponent } from './components/cart-card/cart-card.component';
 import { FavouritesComponent } from './pages/favourites/favourites.component';
 import { AllOwnersComponent } from './pages/all-owners/all-owners.component';
 import { AllOrdersComponent } from './pages/all-orders/all-orders.component';
+import { AllOrderCardComponent } from './components/all-order-card/all-order-card.component';
 
 @NgModule({
   declarations: [
@@ -83,6 +84,7 @@ import { AllOrdersComponent } from './pages/all-orders/all-orders.component';
     FavouritesComponent,
     AllOwnersComponent,
     AllOrdersComponent,
+    AllOrderCardComponent,
   ],
   imports: [
     BrowserModule,
